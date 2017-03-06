c2d7a466764e98d07f0e185783ad9e9a867b555d
Casey Lentz <casey.lentz@OH-CLentz.local>
2017-03-05 21:04:51 -0800

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var src = 'scss';
+var scssMain = src + '/scss-styles.scss';
+var hybrisDest = '../hybris/bin/aramark/arsmvcstore/web/webroot/style/ars';
+var webserverDest = '/Library/WebServer/Documents/style/ars';
+var devUrl = 'http://localhost/webstore/locationoverview.action';
+
+gulp.task('hybrisCSS', function(){
+    return buildCSS(src + '/**/*.scss', hybrisDest);
+});
+
+gulp.task('webserverCSS', function(){
+    return buildCSS(src + '/**/*.scss', webserverDest);
+})
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: devUrl
+    });
+});
+
+gulp.task('reload', function(){
+    browserSync.reload();
+});
+
+gulp.task('watch', ['browser-sync'],function() {
+    gulp.watch(src + '/**/*.scss',['hybrisCSS','webserverCSS','reload']);
+});
+
+function buildCSS(src, dest){
+    return gulp.src(src)
+        .pipe(sass())
+        .pipe(gulp.dest(dest));
+}
